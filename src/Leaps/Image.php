8d90c0f8bef0438b1ba4e5ca7ea52fd884d324f3
XuTongle <xutongle@gmail.com>
2015-02-05 13:42:11 +0800

    增加图像处理库
    Signed-off-by: XuTongle <xutongle@gmail.com>

---
+++ src/Leaps/Image.php
@@ -0,0 +1,29 @@
+<?php
+// +----------------------------------------------------------------------
+// | Leaps Framework [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011-2014 Leaps Team (http://www.tintsoft.com)
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author XuTongle <xutongle@gmail.com>
+// +----------------------------------------------------------------------
+namespace Leaps;
+
+class Image
+{
+	/*
+	 * Resizing constraints
+	 */
+	const NONE = 1;
+	const WIDTH = 2;
+	const HEIGHT = 3;
+	const AUTO = 4;
+	const INVERSE = 5;
+	const PRECISE = 6;
+	const TENSILE = 7;
+
+	// Flipping directions
+	const HORIZONTAL = 11;
+	const VERTICAL = 12;
+}
