123653adba40c499287ab337703ab23e0dd0482f
XuTongle <xutongle@gmail.com>
2015-04-09 15:28:42 +0800

    更新注释
    Signed-off-by: XuTongle <xutongle@gmail.com>

--- src/Leaps/Cache/MemCacheServer.php
+++ src/Leaps/Cache/MemCacheServer.php
@@ -13,48 +13,50 @@ namespace Leaps\Cache;
 class MemCacheServer extends \Leaps\Core\Base
 {
 	/**
-	 *
-	 * @var string memcache server hostname or IP address
+	 * memcache服务器域名或IP地址
+	 * @var string
 	 */
 	public $host;
+
 	/**
-	 *
-	 * @var integer memcache server port
+	 * memcache服务器端口
+	 * @var integer
 	 */
 	public $port = 11211;
+
 	/**
-	 *
-	 * @var integer probability of using this server among all servers.
+	 * 服务器权重（越大使用概率越高 ）
+	 * @var integer
 	 */
 	public $weight = 1;
+
 	/**
-	 *
-	 * @var boolean whether to use a persistent connection. This is used by memcache only.
+	 * 是否使用持久连接。
+	 * @var boolean
 	 */
 	public $persistent = true;
+
 	/**
-	 *
-	 * @var integer timeout in milliseconds which will be used for connecting to the server.
-	 *      This is used by memcache only. For old versions of memcache that only support specifying
-	 *      timeout in seconds this will be rounded up to full seconds.
+	 * 连接超时时间（毫秒）
+	 * @var integer
 	 */
 	public $timeout = 1000;
+
 	/**
-	 *
-	 * @var integer how often a failed server will be retried (in seconds). This is used by memcache only.
+	 * 失败重试时间（秒）
+	 * @var integer
 	 */
 	public $retryInterval = 15;
+
 	/**
-	 *
-	 * @var boolean if the server should be flagged as online upon a failure. This is used by memcache only.
+	 * 标记服务器状态
+	 * @var boolean
 	 */
 	public $status = true;
+
 	/**
-	 *
-	 * @var \Closure this callback function will run upon encountering an error.
-	 *      The callback is run before fail over is attempted. The function takes two parameters,
-	 *      the [[host]] and the [[port]] of the failed server.
-	 *      This is used by memcache only.
+	 * 失败时的回调
+	 * @var \Closure
 	 */
 	public $failureCallback;
 }
